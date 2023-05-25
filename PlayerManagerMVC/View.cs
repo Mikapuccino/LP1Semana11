9a9f5abfc1e822076fc312324323fb351cdb00f9
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-05-25 15:45:13 +0100

    Add PlayerOrder, add ShowMenu method to View class

--- PlayerManagerMVC/View.cs
+++ PlayerManagerMVC/View.cs
@@ -13,5 +13,17 @@ namespace PlayerManagerMVC
         {
             this.controller = controller;
         }
+
+        private void ShowMenu(PlayerOrder playerOrder)
+        {
+            Console.WriteLine("1. Insert new player");
+            Console.WriteLine("2. List all players");
+            Console.WriteLine("3. List all players with a "
+            + "score higher than given value");
+            Console.WriteLine("4. Choose listing option");
+            Console.WriteLine("5. Exit program");
+
+            return int.Parse(Console.ReadLine());
+        }
     }
 }
